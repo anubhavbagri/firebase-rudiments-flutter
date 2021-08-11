03c010e7d584d9c07395dc396e5043cc3e79d805
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2021-08-11 19:34:06 +0530

    feat: add error string constants

---
+++ lib/services/constants.dart
@@ -0,0 +1,3 @@
+const ERROR_1 = 'The account already exists with a different credential';
+const ERROR_2 = 'Error occured while accessing credentials. Try again.';
+const ERROR_3 = 'Error occured using google Sign-In. Try again.';
