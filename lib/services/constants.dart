fb4133090534875cb0758d49b024390cea3a8ec3
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2021-08-11 19:50:59 +0530

    feat: add signout error const

--- lib/services/constants.dart
+++ lib/services/constants.dart
@@ -1,3 +1,5 @@
 const ERROR_1 = 'The account already exists with a different credential';
 const ERROR_2 = 'Error occured while accessing credentials. Try again.';
 const ERROR_3 = 'Error occured using google Sign-In. Try again.';
+
+const SIGNOUT_ERROR = 'Error signing out. Try again.';
