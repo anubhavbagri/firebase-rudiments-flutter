22701173a39e4c63e5f7b3582bec020309454cea
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2021-08-11 15:56:23 +0530

    inital commit

--- flutter_app_1/android/app/src/main/kotlin/com/example/flutter_app_day_1/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_app_day_1
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ todo_app_firebase/android/app/src/main/kotlin/com/example/todo_app_firebase/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.todo_app_firebase
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
