4d54df6c275b4d17a3ef2a701f68cbad140f3654
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2021-08-11 18:38:23 +0530

    feat: random colors to change later

---
+++ lib/utils/custom_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static Color appNavy = Color(0xFFFFFFFF);
+  static Color appCard = Color(0xFFAAAAAA);
+  static Color appYellow = Colors.yellow;
+  static Color appOrange = Colors.orange;
+  static Color appGrey = Colors.grey;
+}
