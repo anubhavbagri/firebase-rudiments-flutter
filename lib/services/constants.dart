e50ec8beffd99fc76f460229935675b28e6db208
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2021-08-11 23:06:09 +0530

    refactor: add remaining error const

--- lib/services/constants.dart
+++ lib/services/constants.dart
@@ -3,3 +3,7 @@ const ERROR_2 = 'Error occured while accessing credentials. Try again.';
 const ERROR_3 = 'Error occured using google Sign-In. Try again.';
 
 const SIGNOUT_ERROR = 'Error signing out. Try again.';
+
+const INIT_ERROR = 'Error initializing Firebase';
+const DISCLAIMER =
+    'You are now signed in using your Google account. To sign out of your account, click the "Sign Out" button below.';
