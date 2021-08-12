f66241bfd5759d3350676e23ceda77505071a540
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2021-08-12 21:43:07 +0530

    feat: add notes model class

---
+++ lib/utils/note_modal.dart
@@ -0,0 +1,20 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Note {
+  final String title;
+  final String? uid;
+  final Timestamp? dateTime;
+  Note({required this.title, this.uid, this.dateTime});
+
+  //this will convert json data to Note object
+  Note.fromJson(Map<String, Object> json)
+      : this(
+            title: json['title'] as String,
+            uid: json['uid'] as String,
+            dateTime: json['dateTime'] as Timestamp);
+
+  //this will convert Note object to Json data(or adding data from our application to firebase)
+  Map<String, Object?> toJson() {
+    return {'title': title, 'uid': uid, 'dateTime': dateTime};
+  }
+}
