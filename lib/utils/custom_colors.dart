6fbce07f73fa62e0e400c642f1fb70e993ca49d5
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2021-08-12 21:42:53 +0530

    style: change color, to be improved later

--- lib/utils/custom_colors.dart
+++ lib/utils/custom_colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class CustomColors {
-  static Color appNavy = Color(0xFFFFFFFF);
+  static Color appNavy = Color(0xFF171717);
   static Color appCard = Color(0xFFAAAAAA);
   static Color appYellow = Colors.yellow;
   static Color appOrange = Colors.orange;
