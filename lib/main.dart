1392dee788a5eeab1642b7fac71dadbcc9eb9711
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2021-08-12 21:38:35 +0530

    feat: add theme data

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:todo_app_firebase/screens/sign_in_screen.dart';
-import 'package:todo_app_firebase/services/size_config.dart';
+import 'package:todo_app_firebase/utils/custom_colors.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,11 +10,20 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // SizeConfig().init(context);
     return GetMaterialApp(
-      title: 'Flutter Demo',
+      title: 'Todo App',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        brightness: Brightness.light,
+        primarySwatch: Colors.orange,
+        bottomAppBarColor: CustomColors.appNavy,
+        appBarTheme: AppBarTheme(
+          backgroundColor: CustomColors.appOrange,
+          centerTitle: true,
+        ),
+        scaffoldBackgroundColor: CustomColors.appNavy,
+        primaryColor: CustomColors.appOrange,
+        bottomNavigationBarTheme: BottomNavigationBarThemeData(
+            backgroundColor: CustomColors.appOrange),
       ),
       debugShowCheckedModeBanner: false,
       home: SignInScreen(),
