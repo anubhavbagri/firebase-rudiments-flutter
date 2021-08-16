7320bf845bbc2a49d9c6daca76665d34e3eaa1fa
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2021-08-16 18:46:06 +0530

    feat: add custom snackbar for showing error

---
+++ lib/custom_widgets/snackbar.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:todo_app_firebase/services/size_config.dart';
+
+class CustomSnackBar {
+  final String message;
+
+  const CustomSnackBar({
+    required this.message,
+  });
+
+  static show(String message) {
+    Get.rawSnackbar(
+      backgroundColor: Colors.white,
+      messageText: Text(
+        message,
+        style: TextStyle(
+          color: Colors.black,
+        ),
+      ), // message
+      mainButton: TextButton(
+        onPressed: () {
+          Get.back();
+        },
+        child: Text(
+          'Close',
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+      ),
+      margin: EdgeInsets.symmetric(
+          vertical: SizeConfig.safeVertical! * .03,
+          horizontal: SizeConfig.safeHorizontal! * .04),
+      borderColor: Colors.red,
+      borderRadius: 10,
+      borderWidth: 2,
+      boxShadows: [
+        BoxShadow(
+          color: Colors.grey,
+          blurRadius: 3.0,
+        )
+      ],
+      isDismissible: false,
+      duration: Duration(hours: 2),
+    );
+  }
+}
