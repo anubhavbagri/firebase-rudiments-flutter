3ece2c45b2f8d18cbe77d60252d7d29ff82fe181
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2021-08-16 21:21:20 +0530

    wip: build notes screen

--- lib/screens/notes.dart
+++ lib/screens/notes.dart
@@ -1,5 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:todo_app_firebase/utils/custom_colors.dart';
+import 'package:todo_app_firebase/utils/database_helper.dart';
+import 'package:todo_app_firebase/utils/note_modal.dart';
 
 class MyNotes extends StatefulWidget {
   const MyNotes({Key? key}) : super(key: key);
@@ -26,7 +30,44 @@ class _MyNotesState extends State<MyNotes> {
         body: Column(
           children: [
             Expanded(
-              child: Container(),
+              child: Container(
+                child: StreamBuilder<QuerySnapshot<Note>>(
+                  stream: Database.notes,
+                  builder: (BuildContext context,
+                      AsyncSnapshot<QuerySnapshot<Note>> snapshot) {
+                    if (snapshot.hasError) {
+                      return Center(
+                        child: Icon(
+                          Icons.report_gmailerrorred_rounded,
+                          size: 80,
+                          color: Colors.orange,
+                        ),
+                      );
+                    }
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return Center(
+                        child: CircularProgressIndicator(
+                          valueColor: AlwaysStoppedAnimation<Color>(
+                              CustomColors.appOrange),
+                        ),
+                      );
+                    } else {
+                      return ListView.builder(
+                          itemCount: snapshot.data!.docs.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            return Dismissible(
+                              key: Key(snapshot.data!.docs[index].id),
+                              background: Container(
+                                color: CustomColors.appNavy,
+                              ),
+                              onDismissed: (direction) {},
+                              child: Card(),
+                            );
+                          });
+                    }
+                  },
+                ),
+              ),
             )
           ],
         ),
