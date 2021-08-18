d165ac9884397d202928ff2f9765099a5d24839b
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2021-08-18 22:13:13 +0530

    feat: new UI for user info screen

--- lib/screens/user_info_screen.dart
+++ lib/screens/user_info_screen.dart
@@ -5,6 +5,7 @@ import 'package:todo_app_firebase/services/constants.dart';
 import 'package:todo_app_firebase/services/size_config.dart';
 import 'package:todo_app_firebase/utils/authentication.dart';
 import 'package:todo_app_firebase/utils/custom_colors.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
 
 class UserInfoScreen extends StatefulWidget {
@@ -32,124 +33,171 @@ class _UserInfoScreenState extends State<UserInfoScreen> {
     SizeConfig().init(context);
     return SafeArea(
       child: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          title: Text('About'),
-        ),
-        body: SafeArea(
-          child: Padding(
-            padding: EdgeInsets.only(
-              left: SizeConfig.safeHorizontal! * .02,
-              right: SizeConfig.safeHorizontal! * .02,
-              bottom: SizeConfig.safeVertical! * .03,
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Stack(
+              alignment: Alignment.bottomCenter,
               children: [
-                Row(),
-                _user!.photoURL != null
-                    ? ClipOval(
-                        child: Material(
-                          color: CustomColors.appGrey.withOpacity(0.3),
-                          child: Image.network(
-                            _user!.photoURL!,
-                            fit: BoxFit.fitHeight,
+                Padding(
+                  padding: const EdgeInsets.all(30.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: CustomColors.peach,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    height: 220.0,
+                    child: Stack(
+                      children: [
+                        Positioned(
+                          left: 2.0,
+                          top: 2.0,
+                          child: IconButton(
+                            icon: Icon(Icons.arrow_back),
+                            color: CustomColors.matte,
+                            onPressed: () {},
                           ),
                         ),
-                      )
-                    : ClipOval(
-                        child: Material(
-                          color: CustomColors.appGrey.withOpacity(0.3),
-                          child: Padding(
-                            padding: EdgeInsets.all(
-                                SizeConfig.safeHorizontal! * .02),
-                            child: Icon(
-                              Icons.account_circle_outlined,
-                              size: 60,
-                              color: CustomColors.appGrey,
-                            ),
-                          ),
+                      ],
+                    ),
+                  ),
+                ),
+                Transform.translate(
+                  offset: Offset(0, 15),
+                  child: Container(
+                    width: 100,
+                    height: 100,
+                    decoration: ShapeDecoration.fromBoxDecoration(
+                      BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(color: Colors.white, width: 2.5),
+                        image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: _user!.photoURL != null
+                              ? NetworkImage(
+                                  _user!.photoURL!,
+                                )
+                              : NetworkImage(
+                                  DUMMY_PROFILE,
+                                ),
                         ),
                       ),
-                SizedBox(height: SizeConfig.safeVertical! * .03),
-                Text(
-                  'Hello',
-                  style: TextStyle(
-                    color: CustomColors.appGrey,
-                    fontSize: 26,
+                    ),
                   ),
                 ),
-                SizedBox(height: SizeConfig.safeVertical! * .03),
-                Text(
-                  _user!.displayName!,
-                  style: TextStyle(
-                    color: CustomColors.appYellow,
-                    fontSize: 26,
-                  ),
-                ),
-                SizedBox(height: SizeConfig.safeVertical! * .03),
-                Text(
-                  '(${_user!.email})',
-                  style: TextStyle(
-                    color: CustomColors.appOrange,
-                    fontSize: 20,
-                    letterSpacing: 0.5,
-                  ),
-                ),
-                SizedBox(height: SizeConfig.safeVertical! * .03),
-                Text(
-                  DISCLAIMER,
-                  style: TextStyle(
-                      color: CustomColors.appGrey.withOpacity(0.8),
-                      fontSize: 14,
-                      letterSpacing: 0.2),
-                ),
-                SizedBox(height: SizeConfig.safeVertical! * .03),
-                _isSigningOut
-                    ? CircularProgressIndicator(
-                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
-                      )
-                    : ElevatedButton(
-                        style: ButtonStyle(
-                          backgroundColor: MaterialStateProperty.all(
-                            Colors.redAccent,
-                          ),
-                          shape: MaterialStateProperty.all(
-                            RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                          ),
-                        ),
-                        onPressed: () async {
-                          setState(() {
-                            _isSigningOut = true;
-                          });
-                          await Authentication.signOut();
-                          setState(() {
-                            _isSigningOut = false;
-                          });
-
-                          Get.off(() => SignInScreen(),
-                              transition: Transition.downToUp);
-                        },
-                        child: Padding(
-                          padding: EdgeInsets.only(
-                              top: SizeConfig.safeVertical! * .01,
-                              bottom: SizeConfig.safeVertical! * .01),
-                          child: Text(
-                            'Sign Out',
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                              letterSpacing: 2,
-                            ),
-                          ),
-                        ),
-                      ),
               ],
             ),
-          ),
+            SizedBox(height: SizeConfig.safeVertical! * .05),
+            Text(
+              _user!.displayName!,
+              overflow: TextOverflow.fade,
+              softWrap: false,
+              style: Theme.of(context).textTheme.headline5!.copyWith(
+                    color: CustomColors.matte,
+                    fontWeight: FontWeight.w700,
+                    fontSize: 30,
+                  ),
+            ),
+            SizedBox(height: SizeConfig.safeVertical! * .01),
+            Text(
+              '${_user!.email}',
+              overflow: TextOverflow.fade,
+              softWrap: false,
+              style: Theme.of(context).textTheme.headline6!.copyWith(
+                    color: CustomColors.matte.withOpacity(0.3),
+                    fontWeight: FontWeight.w500,
+                  ),
+            ),
+            // SizedBox(height: SizeConfig.safeVertical! * .03),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Card(
+                  color: Colors.grey[300],
+                  elevation: 0,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8)),
+                  child: IconButton(
+                    icon: Icon(FontAwesomeIcons.github),
+                    color: CustomColors.matte,
+                    onPressed: () {},
+                  ),
+                ),
+                Card(
+                  color: Colors.grey[300],
+                  elevation: 0,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8)),
+                  child: IconButton(
+                    icon: Icon(FontAwesomeIcons.linkedin),
+                    color: CustomColors.matte,
+                    onPressed: () {},
+                  ),
+                ),
+                Card(
+                  color: Colors.grey[300],
+                  elevation: 0,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8)),
+                  child: IconButton(
+                    icon: Icon(FontAwesomeIcons.behance),
+                    color: CustomColors.matte,
+                    onPressed: () {},
+                  ),
+                ),
+              ],
+            )
+            // Text(
+            //   DISCLAIMER,
+            //   style: TextStyle(
+            //       color: CustomColors.matte.withOpacity(0.3),
+            //       fontSize: 14,
+            //       letterSpacing: 0.2),
+            // ),
+            // SizedBox(height: SizeConfig.safeVertical! * .03),
+            // _isSigningOut
+            //     ? CircularProgressIndicator(
+            //         valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+            //       )
+            //     : ElevatedButton(
+            //         style: ButtonStyle(
+            //           backgroundColor: MaterialStateProperty.all(
+            //             Colors.redAccent,
+            //           ),
+            //           shape: MaterialStateProperty.all(
+            //             RoundedRectangleBorder(
+            //               borderRadius: BorderRadius.circular(10),
+            //             ),
+            //           ),
+            //         ),
+            //         onPressed: () async {
+            //           setState(() {
+            //             _isSigningOut = true;
+            //           });
+            //           await Authentication.signOut();
+            //           setState(() {
+            //             _isSigningOut = false;
+            //           });
+
+            //           Get.off(() => SignInScreen(),
+            //               transition: Transition.downToUp);
+            //         },
+            //         child: Padding(
+            //           padding: EdgeInsets.only(
+            //               top: SizeConfig.safeVertical! * .01,
+            //               bottom: SizeConfig.safeVertical! * .01),
+            //           child: Text(
+            //             'Sign Out',
+            //             style: TextStyle(
+            //               fontSize: 20,
+            //               fontWeight: FontWeight.bold,
+            //               color: Colors.white,
+            //               letterSpacing: 2,
+            //             ),
+            //           ),
+            //         ),
+            //       ),
+          ],
         ),
       ),
     );
