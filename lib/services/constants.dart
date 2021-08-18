dc6a1347ea2f687699746e90915e99a0178c764f
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2021-08-18 16:07:31 +0530

    feat: add sign in info

--- lib/services/constants.dart
+++ lib/services/constants.dart
@@ -1,3 +1,5 @@
+const SIGN_IN_INFO = 'A simple todo app to save\nall your notes in one go!';
+
 const ERROR_1 = 'The account already exists with a different credential';
 const ERROR_2 = 'Error occured while accessing credentials. Try again.';
 const ERROR_3 = 'Error occured using google Sign-In. Try again.';
