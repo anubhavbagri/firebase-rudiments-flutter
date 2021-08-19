29252a18d1aae92e5643a4ed3e5e2cd10997f6e0
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2021-08-19 23:54:21 +0530

    feat: Upgraded entire screen UI

--- lib/screens/notes.dart
+++ lib/screens/notes.dart
@@ -2,10 +2,13 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 import 'package:todo_app_firebase/custom_widgets/snackbar.dart';
 import 'package:todo_app_firebase/screens/create_note.dart';
 import 'package:todo_app_firebase/screens/user_info_screen.dart';
+import 'package:todo_app_firebase/services/size_config.dart';
 import 'package:todo_app_firebase/utils/custom_colors.dart';
 import 'package:todo_app_firebase/utils/database_helper.dart';
 import 'package:todo_app_firebase/utils/note_modal.dart';
@@ -64,111 +67,242 @@ class _MyNotesState extends State<MyNotes> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(
-          title: Text('My Notes'),
-          leading: PopupMenuButton<NoteQuery>(
-            onSelected: _updateNotesQuery,
-            icon: Icon(
-              Icons.sort,
-            ),
-            itemBuilder: (BuildContext context) {
-              return [
-                const PopupMenuItem(
-                  value: NoteQuery.titleAsc,
-                  child: Text('Sort by Title ascending'),
+        body: NestedScrollView(
+          headerSliverBuilder: (context, innerBoxIsScrolled) => [
+            SliverAppBar(
+              expandedHeight: SizeConfig.safeVertical! * .17,
+              flexibleSpace: FlexibleSpaceBar(
+                titlePadding: EdgeInsets.symmetric(
+                  horizontal: SizeConfig.safeHorizontal! * .08,
+                  vertical: SizeConfig.safeVertical! * .02,
                 ),
-                const PopupMenuItem(
-                  value: NoteQuery.titleDesc,
-                  child: Text('Sort by Title descending'),
+                title: Text(
+                  'Notes',
+                  style: Theme.of(context).textTheme.headline5!.copyWith(
+                        color: CustomColors.matte,
+                        fontWeight: FontWeight.w700,
+                      ),
                 ),
-                const PopupMenuItem(
-                  value: NoteQuery.dateAsc,
-                  child: Text('Sort by Date ascending'),
-                ),
-                const PopupMenuItem(
-                  value: NoteQuery.dateDesc,
-                  child: Text('Sort by Date descending'),
-                ),
-              ];
-            },
-          ),
-          actions: [
-            IconButton(
-              icon: Icon(
-                Icons.settings,
               ),
-              onPressed: () {
-                Get.to(() => UserInfoScreen(user: widget._user));
-              },
-            )
-          ],
-        ),
-        body: Column(
-          children: [
-            Expanded(
-              child: Container(
-                child: StreamBuilder<QuerySnapshot<Note>>(
-                  stream: _notes,
-                  builder: (BuildContext context,
-                      AsyncSnapshot<QuerySnapshot<Note>> snapshot) {
-                    if (snapshot.hasError) {
-                      return Center(
-                        child: Icon(
-                          Icons.report_gmailerrorred_rounded,
-                          size: 80,
-                          color: Colors.orange,
-                        ),
-                      );
-                    }
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return Center(
-                        child: CircularProgressIndicator(
-                          valueColor: AlwaysStoppedAnimation<Color>(
-                              CustomColors.orange),
-                        ),
-                      );
-                    } else {
-                      return ListView.builder(
-                        itemCount: snapshot.data!.docs.length,
-                        itemBuilder: (BuildContext context, int index) {
-                          return Dismissible(
-                            key: Key(snapshot.data!.docs[index].id),
-                            background: Container(
-                              color: CustomColors.matte,
-                            ),
-                            onDismissed: (direction) {
-                              Database.deleteNoteById(
-                                  snapshot.data!.docs[index].id);
-                              CustomSnackBar.show('Note dismissed');
-                            },
-                            child: Card(
-                              color: Colors.white10,
-                              child: ListTile(
-                                title: Text(
-                                  snapshot.data!.docs[index].data().title,
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                                onTap: () async {
-                                  ///storing document of a particular document id
-                                  DocumentReference docRef = Database.notesRef
-                                      .doc(snapshot.data!.docs[index].id);
-                                  DocumentSnapshot<Note> docSnap = await docRef
-                                      .get() as DocumentSnapshot<Note>;
-
-                                  ///navigate to createNote screen
-                                  Get.to(CreateNote(document: docSnap));
-                                },
-                              ),
-                            ),
-                          );
-                        },
-                      );
-                    }
+              actions: [
+                PopupMenuButton<NoteQuery>(
+                  onSelected: _updateNotesQuery,
+                  icon: Icon(
+                    Icons.sort_rounded,
+                    size: 34.0,
+                  ),
+                  itemBuilder: (BuildContext context) {
+                    return [
+                      const PopupMenuItem(
+                        value: NoteQuery.titleAsc,
+                        child: Text('Sort by Title ascending'),
+                      ),
+                      const PopupMenuItem(
+                        value: NoteQuery.titleDesc,
+                        child: Text('Sort by Title descending'),
+                      ),
+                      const PopupMenuItem(
+                        value: NoteQuery.dateAsc,
+                        child: Text('Sort by Date ascending'),
+                      ),
+                      const PopupMenuItem(
+                        value: NoteQuery.dateDesc,
+                        child: Text('Sort by Date descending'),
+                      ),
+                    ];
                   },
                 ),
-              ),
-            )
+                IconButton(
+                  icon: Icon(
+                    Icons.settings,
+                    size: 28,
+                  ),
+                  onPressed: () {
+                    Get.to(() => UserInfoScreen(user: widget._user));
+                  },
+                )
+              ],
+            ),
           ],
+          body: Column(
+            children: [
+              Expanded(
+                child: Container(
+                  child: StreamBuilder<QuerySnapshot<Note>>(
+                    stream: _notes,
+                    builder: (BuildContext context,
+                        AsyncSnapshot<QuerySnapshot<Note>> snapshot) {
+                      if (snapshot.hasError) {
+                        return Center(
+                          child: Icon(
+                            FontAwesomeIcons.exclamationTriangle,
+                            size: 80,
+                            color: CustomColors.orange,
+                          ),
+                        );
+                      }
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return Center(
+                          child: CircularProgressIndicator(
+                            valueColor: AlwaysStoppedAnimation<Color>(
+                                CustomColors.orange),
+                          ),
+                        );
+                      } else if (snapshot.data!.docs.length == 0) {
+                        return Center(
+                          child: Text(
+                            'Start by adding your first note!',
+                            style: TextStyle(
+                                color: CustomColors.matte.withOpacity(0.3)),
+                          ),
+                        );
+                      } else {
+                        return ListView.builder(
+                          physics: const BouncingScrollPhysics(
+                              parent: AlwaysScrollableScrollPhysics()),
+                          itemCount: snapshot.data!.docs.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            return Dismissible(
+                              key: Key(snapshot.data!.docs[index].id),
+                              background: Container(
+                                alignment: AlignmentDirectional.centerEnd,
+                                color: Colors.transparent,
+                                child: Padding(
+                                  padding:
+                                      EdgeInsets.fromLTRB(0.0, 0.0, 30.0, 0.0),
+                                  child: Icon(
+                                    Icons.delete,
+                                    size: 32,
+                                    color: CustomColors.orange,
+                                  ),
+                                ),
+                              ),
+                              direction: DismissDirection.endToStart,
+                              onDismissed: (direction) {
+                                Database.deleteNoteById(
+                                    snapshot.data!.docs[index].id);
+                                CustomSnackBar.show('Note dismissed');
+                              },
+                              child: Padding(
+                                padding: EdgeInsets.symmetric(
+                                  horizontal: SizeConfig.safeHorizontal! * .07,
+                                  vertical: SizeConfig.safeVertical! * .01,
+                                ),
+                                child: Stack(
+                                  children: [
+                                    GestureDetector(
+                                      onTap: () async {
+                                        /// storing document of a particular document id
+                                        DocumentReference docRef = Database
+                                            .notesRef
+                                            .doc(snapshot.data!.docs[index].id);
+                                        DocumentSnapshot<Note> docSnap =
+                                            await docRef.get()
+                                                as DocumentSnapshot<Note>;
+
+                                        ///navigate to createNote screen
+                                        Get.to(CreateNote(document: docSnap));
+                                      },
+                                      child: Container(
+                                        height: SizeConfig.safeVertical! * .18,
+                                        width: double.infinity,
+                                        child: Padding(
+                                          padding: EdgeInsets.only(
+                                              left: SizeConfig.safeHorizontal! *
+                                                  .1,
+                                              top: SizeConfig.safeVertical! *
+                                                  .01),
+                                          child: Text(
+                                            snapshot.data!.docs[index]
+                                                .data()
+                                                .title,
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .subtitle1!
+                                                .copyWith(
+                                                  color: CustomColors.orange,
+                                                  fontWeight: FontWeight.w600,
+                                                ),
+                                            // overflow: TextOverflow.fade,
+                                            // softWrap: false,
+                                          ),
+                                        ),
+                                        decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(20),
+                                          gradient: LinearGradient(
+                                            colors: [
+                                              CustomColors.peach,
+                                              CustomColors.peach,
+                                              CustomColors.orange,
+                                              CustomColors.orange
+                                            ],
+                                            stops: [0.0, 0.87, 0.87, 1.0],
+                                            end: Alignment.topLeft,
+                                            begin: Alignment.topCenter,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                    Positioned(
+                                      left: SizeConfig.safeHorizontal! * .09,
+                                      bottom: SizeConfig.safeVertical! * .01,
+                                      child: Row(
+                                        children: [
+                                          Card(
+                                            color:
+                                                Colors.white.withOpacity(0.5),
+                                            elevation: 0,
+                                            shape: RoundedRectangleBorder(
+                                                borderRadius:
+                                                    BorderRadius.circular(5)),
+                                            child: Padding(
+                                              padding:
+                                                  const EdgeInsets.all(8.0),
+                                              child: Text(
+                                                '${DateFormat("d MMM y").format(snapshot.data!.docs[index].data().dateTime!.toDate())}',
+                                                style: Theme.of(context)
+                                                    .textTheme
+                                                    .subtitle2!
+                                                    .copyWith(
+                                                      color:
+                                                          CustomColors.orange,
+                                                      fontWeight:
+                                                          FontWeight.w600,
+                                                    ),
+                                              ),
+                                            ),
+                                          ),
+                                          SizedBox(
+                                            width: SizeConfig.safeHorizontal! *
+                                                .05,
+                                          ),
+                                          Text(
+                                            '${DateFormat("jm").format(snapshot.data!.docs[index].data().dateTime!.toDate())}',
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .subtitle2!
+                                                .copyWith(
+                                                  color: CustomColors.orange,
+                                                  fontWeight: FontWeight.w600,
+                                                ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            );
+                          },
+                        );
+                      }
+                    },
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
         floatingActionButton: FloatingActionButton(
           backgroundColor: CustomColors.orange,
