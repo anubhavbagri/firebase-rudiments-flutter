c0298b3ea991957db64bd53bf94e16f010de407c
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2021-08-19 23:53:12 +0530

    feat: storing user name in firestore

--- lib/utils/note_modal.dart
+++ lib/utils/note_modal.dart
@@ -2,9 +2,10 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Note {
   final String title;
+  final String? name;
   final String? uid;
   final Timestamp? dateTime;
-  Note({required this.title, this.uid, this.dateTime});
+  Note({required this.title, this.name, this.uid, this.dateTime});
 
   //this will convert json data to Note object
   Note.fromJson(Map<String, Object?> json)
@@ -15,6 +16,11 @@ class Note {
 
   //this will convert Note object to Json data(or adding data from our application to firebase)
   Map<String, Object?> toJson() {
-    return {'title': title, 'uid': uid, 'dateTime': dateTime};
+    return {
+      'title': title,
+      'name': name,
+      'uid': uid,
+      'dateTime': dateTime,
+    };
   }
 }
