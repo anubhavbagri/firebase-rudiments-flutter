1d7e78c91dde21ad90f4fdc213599ba0afaf70a7
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2021-08-19 16:24:20 +0530

    feat: new buttons UI

--- lib/screens/create_note.dart
+++ lib/screens/create_note.dart
@@ -1,7 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
+import 'package:todo_app_firebase/services/size_config.dart';
+import 'package:todo_app_firebase/utils/custom_colors.dart';
 import 'package:todo_app_firebase/utils/database_helper.dart';
 import 'package:todo_app_firebase/utils/note_modal.dart';
 
@@ -22,23 +25,40 @@ class CreateNote extends StatelessWidget {
         resizeToAvoidBottomInset: false,
         appBar: AppBar(
           title: document == null
-              ? Text('Create a New Note')
-              : Text('Update note'),
+              ? Text(
+                  'Create a New Note',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                  ),
+                )
+              : Text(
+                  'Update note',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
         ),
         backgroundColor: Colors.white,
         body: Column(
           children: [
             Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+              padding: EdgeInsets.symmetric(
+                horizontal: SizeConfig.safeHorizontal! * .04,
+                vertical: SizeConfig.safeVertical! * .02,
+              ),
               child: TextField(
                 controller: textEditingController,
                 autofocus: true,
                 textCapitalization: TextCapitalization.sentences,
+                cursorColor: CustomColors.peach,
                 decoration: InputDecoration.collapsed(
                   hintText: 'What do you want to save today?',
-                  // hintStyle:
+                  hintStyle: Theme.of(context).textTheme.headline6!.copyWith(
+                        color: CustomColors.matte.withOpacity(0.3),
+                        fontWeight: FontWeight.w400,
+                      ),
                 ),
-                style: TextStyle(fontSize: 20),
+                style: TextStyle(fontSize: 22),
                 keyboardType: TextInputType.multiline,
                 // minLines: 1,
                 maxLines: 5,
@@ -46,46 +66,58 @@ class CreateNote extends StatelessWidget {
             ),
             Spacer(),
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                OutlinedButton(
-                  onPressed: () {
-                    Get.back();
-                  },
-                  child: Text('Cancel'),
-                  style: OutlinedButton.styleFrom(primary: Colors.red),
+                Card(
+                  color: Colors.grey[300],
+                  elevation: 0,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  child: IconButton(
+                    icon: Icon(FontAwesomeIcons.times),
+                    color: CustomColors.matte.withOpacity(0.8),
+                    iconSize: 26,
+                    onPressed: () {
+                      Get.back();
+                    },
+                  ),
                 ),
-                ElevatedButton(
-                  onPressed: () {
-                    Timestamp myTimeStamp = Timestamp.fromDate(DateTime.now());
-                    if (document == null) {
-                      Database.addNote(
-                        Note(
+                Card(
+                  color: CustomColors.peach,
+                  elevation: 0,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  child: IconButton(
+                    icon: Icon(FontAwesomeIcons.check),
+                    color: CustomColors.matte.withOpacity(0.8),
+                    iconSize: 26,
+                    onPressed: () {
+                      Timestamp myTimeStamp =
+                          Timestamp.fromDate(DateTime.now());
+                      if (document == null) {
+                        Database.addNote(
+                          Note(
+                              title: textEditingController.text,
+                              uid: Database.user!.uid,
+                              dateTime: myTimeStamp),
+                        );
+                      } else {
+                        Database.updateNoteById(
+                          document!.id,
+                          Note(
                             title: textEditingController.text,
-                            uid: Database.user!.uid,
-                            dateTime: myTimeStamp),
-                      );
-                    } else {
-                      Database.updateNoteById(
-                        document!.id,
-                        Note(
-                          title: textEditingController.text,
-                          dateTime: myTimeStamp,
-                        ),
-                      );
-                    }
-                    Get.back();
-                  },
-                  child: document == null ? Text('Add') : Text('Update'),
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.green,
-                    elevation: 2,
+                            dateTime: myTimeStamp,
+                          ),
+                        );
+                      }
+                      Get.back();
+                    },
                   ),
                 ),
               ],
             ),
             SizedBox(
-              height: 10,
+              height: SizeConfig.safeVertical! * .04,
             ),
             Padding(
               padding: EdgeInsets.only(
