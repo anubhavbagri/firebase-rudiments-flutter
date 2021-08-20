d4e8f3250a975f8d6bc7d911772a58fc86027d9f
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2021-08-20 18:42:36 +0530

    feat: add empty screen _disclaimer_

--- lib/services/constants.dart
+++ lib/services/constants.dart
@@ -1,4 +1,5 @@
 const SIGN_IN_INFO = 'A simple todo app to save\nall your notes in one go!';
+const EMPTY_INFO = 'Click on ➕ button to add your first note!';
 
 const ERROR_1 = 'The account already exists with a different credential';
 const ERROR_2 = 'Error occured while accessing credentials. Try again.';
